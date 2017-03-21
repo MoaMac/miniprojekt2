10ed5aa9902dd35bc782e89fa7718dac7ee1d915
robineriksson86 <robinaeriksson86@gmail.com>
2017-03-21 09:17:44 +0100

    ij setup

---
+++ src/main/java/com/example/Miniproekt2Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Miniproekt2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Miniproekt2Application.class, args);
+	}
+}
